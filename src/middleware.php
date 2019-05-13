aec714a365c10a8cf6c47681e6ee101783187a61
Ivans <ivans1041@gmail.com>
2019-05-13 11:07:52 -0400

    build: production

--- src/middleware.php
+++ src/middleware.php
@@ -29,7 +29,7 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 $app->add(function ($req, $res, $next) {
   $response = $next($req, $res);
   return $response
-    ->withHeader('Access-Control-Allow-Origin', 'http://www.gardensofamerica.com')
+    ->withHeader('Access-Control-Allow-Origin', '*')
     ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization, token')
     ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
